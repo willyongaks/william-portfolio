e356d3e9d235b208a2d859b9a5774d9490be3f9e
willyongaks <william.ongaki10@gmail.com>
2023-03-01 14:42:13 +0100

    n

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './components/pages/Home';
-import About from './components/pages/About';
-import Contact from './components/pages/Contact';
+import Home from './components/pages/home/Home';
+import About from './components/pages/about/About';
+import Contact from './components/pages/contact/Contact';
+import Menu from './components/navigation/Menu';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Menu />
         <Routes>
           <Route path="/" exact element={<Home />} />
-          <Route path="about"  element={ <About />}/>
-          <Route path="contact"  element={ <Contact /> }/>
+          <Route path="/about"  element={ <About />}/>
+          <Route path="/contact"  element={ <Contact /> }/>
         </Routes>
       </BrowserRouter> 
     </div>
---
+++ src/components/navigation/Menu.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+// import styled from 'styled-components/macro';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+
+
+
+// const Logo = styled.div`
+//   margin-top: 30px;
+//   width: 70px;
+//   height: 70px;
+// `
+
+function Menu() {
+
+  return (
+
+    <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Link to="/" className='nav-link'>Home</Link>
+            <Link to="/about" className='nav-link'>About</Link>
+            <Link to="/contact" className='nav-link'>Contact</Link>
+          </Nav>
+        </Container>
+    </Navbar>
+    
+  )
+}
+
+export default Menu
