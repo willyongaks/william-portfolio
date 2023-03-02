1687d2542ca71209cf6fe8dc4f1bde52964a6dd9
willyongaks <83573335+willyongaks@users.noreply.github.com>
2023-03-02 15:10:36 +0100

    n

--- src/components/navigation/Menu.js
+++ src/components/navigation/Menu.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/logo.png';
 
 
 
@@ -21,7 +22,9 @@ function Menu() {
 
     <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/">
+          <img src={logo} alt='logo'/>
+          </Navbar.Brand>
           <Nav className="mr-auto">
             <Link to="/" className='nav-link'>Home</Link>
             <Link to="/about" className='nav-link'>About</Link>
