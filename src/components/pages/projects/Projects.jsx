8597080d30a9eeb4667c0b88c3946d5402550c96
willyongaks <william.ongaki10@gmail.com>
2023-03-03 17:58:00 +0100

    n

--- src/components/pages/projects/Projects.jsx
+++ src/components/pages/projects/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components';
 
 
+
 const ProjectSection = styled.section`
     display: flex;
     flex-direction: column;
@@ -17,12 +18,30 @@ const ProjectHeader = styled.h1`
 const TabContent = styled.div`
     margin: 1.5em 0px;
 `;
-
+const ProjCard = styled.div`
+    margin: 0px 0px 1.5em;
+    osition: relative;
+    cursor: pointer;
+    object-fit: cover;
+    background-color: var(--secondary-color);
+    background-position: center center;
+    background-repeat: no-repeat;
+    overflow: hidden;
+    padding: 1.4rem 2rem;
+    display: flex;
+    align-items: flex-end;
+    border-radius: 9px;
+    break-inside: avoid;
+`;
 function Projects() {
   return (
     <ProjectSection>
         <ProjectHeader>My recent projects</ProjectHeader>
-        <TabContent></TabContent>
+        <TabContent>
+            <ProjCard>
+
+            </ProjCard>
+        </TabContent>
     </ProjectSection>
   )
 }
