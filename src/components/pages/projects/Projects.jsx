b4a0102ec1f6638567611db5137cdbdfc5adf501
willyongaks <william.ongaki10@gmail.com>
2023-03-03 18:18:20 +0100

    Update Projects.jsx

--- src/components/pages/projects/Projects.jsx
+++ src/components/pages/projects/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
+import SosholsImage from '../../assets/soshols.png'
 
 
 
@@ -19,27 +20,56 @@ const TabContent = styled.div`
     margin: 1.5em 0px;
 `;
 const ProjCard = styled.div`
-    margin: 0px 0px 1.5em;
-    osition: relative;
-    cursor: pointer;
-    object-fit: cover;
-    background-color: var(--secondary-color);
-    background-position: center center;
-    background-repeat: no-repeat;
-    overflow: hidden;
-    padding: 1.4rem 2rem;
     display: flex;
-    align-items: flex-end;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 500px;
+    height: 300px;
     border-radius: 9px;
-    break-inside: avoid;
+    overflow: hidden;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
 `;
+
+const ProjCardImage = styled.img`
+    width: 300px;
+    height: 100%;
+    object-fit: cover;
+    
+`;
+
+const ProjCardContent = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+const ProjCardTitle = styled.p`
+    color: var(--text-color);
+    margin: 0;
+    font-size: var(--medium-font);
+`;
+
+const ProjCardDescription = styled.p`
+    color: var(--text-color);
+    margin: 0.5em 0 0;
+`;
+
+
+
+
+
 function Projects() {
   return (
     <ProjectSection>
         <ProjectHeader>My recent projects</ProjectHeader>
         <TabContent>
             <ProjCard>
-
+                <ProjCardImage src={SosholsImage} alt="Project" />
+                <ProjCardContent>
+                    <ProjCardTitle>Soshols</ProjCardTitle>
+                    <ProjCardDescription>This is a social media project that i was working on</ProjCardDescription>
+                </ProjCardContent>
             </ProjCard>
         </TabContent>
     </ProjectSection>
