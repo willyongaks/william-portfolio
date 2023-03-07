53066046fdc2a005497846f48ae1a566f903a18b
willyongaks <william.ongaki10@gmail.com>
2023-03-07 11:44:52 +0100

    Update Home.jsx

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -31,7 +31,12 @@ function Home() {
                     i'm William Ongaki
                   </Header>
                   <HeaderText>
-                    I am a frontend developer based in Oslo
+                    I am a frontend developer based in Oslo with a passion for creating user-friendly
+                    experiences on the web. As a developer, I strive to not only meet but exceed user expectations
+                    by creating visually appealing and intuitive interfaces that enhance the overall user experience.
+                    Through my work, I aim to provide efficient and seamless digital experiences that engage and delight users.
+                    I am constantly seeking to learn and grow in my field, and I am excited to bring my skills and enthusiasm to 
+                    new projects and challenges.
                   </HeaderText>
                 </article>
                 <HeroButton href='/about'>
