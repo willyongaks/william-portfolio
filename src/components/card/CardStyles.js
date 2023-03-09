3603921b19fabb4b191bd6d62c976e17f893330f
willyongaks <william.ongaki10@gmail.com>
2023-03-09 17:50:13 +0100

    Update CardStyles.js

--- src/components/card/CardStyles.js
+++ src/components/card/CardStyles.js
@@ -26,7 +26,7 @@ export const GridCard = styled.article`
   flex-direction: column;
   justify-content: space-between;
   align-items: stretch;
-  border-radius: 1rem;
+  border-radius: 0.5rem 0.5rem;
   border: 1px solid var(--text-color);
   box-shadow: 0 0.1rem 0.1rem rgb(0 0 0 / 14%), 0 0.2rem 0.1rem rgb(0 0 0 / 12%), 0 0.1rem 0.3rem rgb(0 0 0 / 20%);
 `;
@@ -43,8 +43,6 @@ export const GridCardText = styled.div`
   padding: 2.5rem 2rem 2rem 2rem;
   font-size: 1.5rem;
   flex: auto;
-  // border-left: 1px solid var(--text-color);
-  // border-right: 1px solid var(--text-color);
 `;
 
 export const CardTextTitle = styled.h3`
