f74120bd66bb8e68d130f2e64f229790ac87302c
willyongaks <william.ongaki10@gmail.com>
2023-03-09 17:21:21 +0100

    n

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useRef} from 'react';
+import React, { useRef, useState} from 'react';
 import emailjs from '@emailjs/browser';
 import { Col, Container, Row } from 'react-bootstrap';
 import { 
@@ -10,10 +10,13 @@ import {
   FormInput,
   FormTextarea,
   SendButton,
-  // Message,
+  Message,
+  MessageLink,
 } from './ContactStyles';
+import { BsArrowRight } from "react-icons/bs";
 
 function Contact() {
+  const[formSubmited, setFormSubmited] = useState(false);
    const form = useRef();
 
   const sendEmail = (e) => {
@@ -21,6 +24,7 @@ function Contact() {
 
     emailjs.sendForm('service_67y8wu2', 'template_3kq9psp', form.current, 'U6by-e20Z0xIhjVwk')
       .then((result) => {
+        setFormSubmited(true);
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
@@ -29,51 +33,59 @@ function Contact() {
 
 
   return (
-    <MainContent>
-      <Container>
-        <Row>
-          <Col>
-          <ContactHeading>Contact</ContactHeading>
-          <article>
-            <ArticleText>
-              Get in touch or send me an email at 
-              <b>william.ongaki10@gmail.com</b>
-            </ArticleText>
-          </article>
-          <FormContainer ref={form} onSubmit={sendEmail}>
-            <FormFields>
-              <FormInput 
-                type="text"
-                name="user_name"
-                placeholder=" Name"
-                className='FromControl'
-                required
-              />
-              <FormInput 
-                type="email"
-                name="user_email"
-                placeholder=" Email"
-                className='FromControl'
-                required
-              />
-              <FormTextarea 
-                name="message"
-                placeholder=" Message"
-                aria-required="true"
-                rows={5}
-                required
-
-              />
-            </FormFields>
-            <SendButton
-            type='submit'
-            value='send'
-          > Send message</SendButton>
-          </FormContainer>
-          </Col>
-        </Row>
-      </Container>
-    </MainContent>
+    <>
+    {formSubmited ? (
+      <Message className='container'>
+        Thank you for your message. We will get back to you as soon as possible
+        <MessageLink to='/'>
+          Go back home <BsArrowRight />
+        </MessageLink>
+      </Message>
+    ) : (
+      <MainContent>
+          <Container>
+            <Row>
+              <Col>
+              <ContactHeading>Contact</ContactHeading>
+              <article>
+                <ArticleText>
+                  Get in touch or send me an email at 
+                  <b>william.ongaki10@gmail.com</b>
+                </ArticleText>
+              </article>
+                <FormContainer ref={form} onSubmit={sendEmail}>
+                  <FormFields>
+                    <FormInput 
+                      type="text"
+                      name="user_name"
+                      placeholder=" Name"
+                      className='FromControl'
+                      required
+                    />
+                    <FormInput 
+                      type="email"
+                      name="user_email"
+                      placeholder=" Email"
+                      className='FromControl'
+                      required
+                    />
+                    <FormTextarea 
+                      name="message"
+                      placeholder=" Message"
+                      aria-required="true"
+                      rows={5}
+                      required
+                    />
+                    </FormFields>
+                    <SendButton type='submit' value='send'> Send message <BsArrowRight /></SendButton>
+                </FormContainer>
+              </Col>
+            </Row>
+          </Container>
+      </MainContent>
+    )}
+    </>
+    
   )
 }
 
--- src/components/pages/contact/ContactStyles.js
+++ src/components/pages/contact/ContactStyles.js
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import styled, { keyframes} from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const MainContent = styled.section`
   display: block;
@@ -22,79 +23,90 @@ export const ArticleText = styled.p`
 `;
 
 export const FormContainer = styled.form`
-    width: 60%;
+  width: 60%;
 
-    @media (max-width: 768px) {
-        width: 100%;
-    }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 export const FormFields = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2rem;
 `;
 
 
 export const FormInput = styled.input`
-    background: transparent;
-    resize: none;
-    color: var(--text-color) !important;
-    box-shadow: none !important;
-    height: calc(1.5em + 0.75rem + 2px);
-    padding: 24px 21px !important;
-    border: 1px solid var(--secondary-color);
-    border-radius: 0.25rem;
-    box-shadow: none !important;
-    outline: none;
-    width: 100%;
-    font-weight: 400;
-    line-height: 1.5;
+  background: transparent;
+  resize: none;
+  color: var(--text-color) !important;
+  box-shadow: none !important;
+  height: calc(1.5em + 0.75rem + 2px);
+  padding: 24px 21px !important;
+  border: 1px solid var(--secondary-color);
+  border-radius: 0.25rem;
+  box-shadow: none !important;
+  outline: none;
+  width: 100%;
+  font-weight: 400;
+  line-height: 1.5;
    
 `;
 
 export const FormTextarea = styled.textarea`
-    box-shadow: none !important;
-    padding: 24px 21px !important;
-    background: transparent;
-    margin-bottom: 2rem;
-    font-size: 1rem;
-    font-weight: 400;
-    line-height: 1.5;
-    color: var(--text-color);
-    resize: none;
-    outline: none;
-    border: 1px solid var(--secondary-color);
-    border-radius: 0.25rem;
-    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+  box-shadow: none !important;
+  padding: 24px 21px !important;
+  background: transparent;
+  margin-bottom: 2rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: var(--text-color);
+  resize: none;
+  outline: none;
+  border: 1px solid var(--secondary-color);
+  border-radius: 0.25rem;
+  transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
   
 `;
 export const Message = styled.p`
-  margin: 10px 0;
   padding: 10px;
   border-radius: 5px;
   width: 60%;
-
-  &.success {
-    background-color: #d4edda;
-    color: #155724;
-    border: 1px solid #c3e6cb;
-  }
-
-  &.error {
-    background-color: #f8d7da;
-    color: #721c24;
-    border: 1px solid #f5c6cb;
-  }
+  color: var(--secondary-color);
 `;
 
+const move = keyframes`
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(10px);
+  }
+`;
 export const SendButton = styled.button`
-    font-size: calc(var(--extra-small-font) + 1.1px);
-    background-color: var(--secondary-color);
-    border-radius: 5px;
-    border: none;
-    padding: 15px 40px;
-    margin-bottom: 3rem;
+  font-size: calc(var(--extra-small-font) + 1.1px);
+  background-color: var(--secondary-color);
+  border-radius: 5px;
+  border: none;
+  padding: 15px 40px;
+  margin-bottom: 3rem;
+
+  svg {
+    margin-left: 1rem;
+    animation: ${move} 0.5s alternate infinite;
+  }
   
+`;
+export const MessageLink = styled(Link)`
+  color: var(--text-color);
+  text-decoration: none;
+  padding-left: 1em;
+
+  svg {
+    margin-left: 1rem;
+    animation: ${move} 0.5s alternate infinite;
+  }
 `;
